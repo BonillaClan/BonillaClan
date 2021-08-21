0ed24bd377cceef7fe0093201e6f6bf5425b46f9
Abonilla17 <alexanderbjr@gmail.com>
2021-08-20 16:06:01 -1000

    actually added password check

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+var password;
+
+var pass1="cool";
+
+password=prompt('Please enter your password to view this page!',' ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjqnMGZgsHyAhUjL30KHc-1AEMQwqsBegQIBxAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU";
+    }
+
+
+
+
+
